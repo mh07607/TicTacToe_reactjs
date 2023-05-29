c06158c50ff08ac01c9c2be0aaff1ec767c35d62
mh07607 <mh07607@st.habib.edu.pk>
2023-05-30 04:47:17 +0500

    classes defined

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,15 +1,50 @@
 import { Component } from 'react'
 
+function onBtnClick(count){
+    if(count % 2 === 0){
+      return(
+          <button className="circle">
+              O
+          </button>
+      );
+    } else {
+      return(
+          <button className="square">
+              X
+          </button>
+      );
+    } 
+}
+
+class Board extends Component{
+  constructor(props){
+    super(props);
+  }
+}
+
 export default class App extends Component{
     constructor(){
       super();
+
+      this.state = {
+        count: 0, 
+      };
+
+      this.countUp = this.countUp.bind(this);
     }
+
+    countUp = () => {
+      this.setState({
+        count: this.state.count + 1
+      });
+    };
 
     render(){
         return(
-            <div>
+            <>
                 <h1>React App</h1>
-            </div>
+                <Square />
+            </>
         );
     }
 }
