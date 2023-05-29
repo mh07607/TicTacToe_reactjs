5b0f1703746fcadc48bd483cf6ca91d028d8dca4
mh07607 <mh07607@st.habib.edu.pk>
2023-05-30 01:11:32 +0500

    set up (2)

---
+++ tic-tac-toe/src/App.js
@@ -0,0 +1,15 @@
+import { Component } from 'react'
+
+export default class App extends Component{
+    constructor(){
+      super();
+    }
+
+    render(){
+        return(
+            <div>
+                <h1>React App</h1>
+            </div>
+        );
+    }
+}
