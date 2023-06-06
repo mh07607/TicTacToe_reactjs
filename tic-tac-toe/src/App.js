f38ad9fe257fc6b064d9381573ac199f322f39aa
mh07607 <mh07607@st.habib.edu.pk>
2023-06-06 13:07:07 +0500

    starting over because cannot understand

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,49 +1,16 @@
 import { Component } from 'react'
 
-function onBtnClick(count){
-    if(count % 2 === 0){
-      return(
-          <button className="circle">
-              O
-          </button>
-      );
-    } else {
-      return(
-          <button className="square">
-              X
-          </button>
-      );
-    } 
-}
 
-class Board extends Component{
-  constructor(props){
-    super(props);
-  }
-}
 
 export default class App extends Component{
     constructor(){
       super();
-
-      this.state = {
-        count: 0, 
-      };
-
-      this.countUp = this.countUp.bind(this);
     }
-
-    countUp = () => {
-      this.setState({
-        count: this.state.count + 1
-      });
-    };
-
+    
     render(){
         return(
             <>
                 <h1>React App</h1>
-                <Square />
             </>
         );
     }
