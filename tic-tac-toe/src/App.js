579c5e34d89e118c4b79cbdbbcaf06788e52c74e
mh07607 <mh07607@st.habib.edu.pk>
2023-06-07 18:58:49 +0500

    tictactoe with timetravel implemented

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -3,12 +3,26 @@ import { Board } from './components/Board.js'
 
 export default function Game(){
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  const [XisNext, setXisNext] = useState(true);
-  const currentGrid = history[history.length-1];
+  const [currentMove, setCurrentMove] = useState(0);
+  const currentGrid = history[currentMove];
+  const XisNext = currentMove%2==0;
+
+  const moves = history.map((squares, move)=>{
+    return (
+      <li key={move}>
+        <button onClick={() => {jumpToMove(move)}}>Go to move {move}</button>
+      </li>
+    );
+  })
+
+  function jumpToMove(move){
+    setCurrentMove(move);
+  }
 
   function onPlay(nextSquares){
-    setHistory([...history, nextSquares]);
-    setXisNext(!XisNext);
+    const nextHistory = [...history.slice(0, currentMove+1), nextSquares]
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length-1);
   }
 
   return(
@@ -17,7 +31,9 @@ export default function Game(){
         <Board squares = {currentGrid} XisNext = {XisNext} onPlay = {onPlay}/>
       </div>
       <div className='allMoves'>
-        
+        <ol>
+          {moves}
+        </ol>
       </div>
     </div>
   );
