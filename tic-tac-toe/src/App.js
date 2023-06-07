f5676ae5f0d5f87b6326669164eca24d9a5b19a9
mh07607 <mh07607@st.habib.edu.pk>
2023-06-07 16:21:00 +0500

    X and O functionality

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,17 +1,52 @@
-import { Component } from 'react'
+import { Component, useState } from 'react'
+
+
+export function Square(props){
+  return (
+    <button className='square' onClick={props.onBtnClick}>{props.value}</button>
+  );
+}
 
 
 
-export default class App extends Component{
-    constructor(){
-      super();
+export default function Board(){
+  const [count, setCount] = useState(0);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(i){
+    if(squares[i]){
+      return;
     }
-    
-    render(){
-        return(
-            <>
-                <h1>React App</h1>
-            </>
-        );
+
+    let newSquares = squares;
+
+    if(count % 2 == 0){
+      newSquares[i] = 'X';
+    } else {
+      newSquares[i] = 'O';
     }
+
+    setSquares(newSquares);
+    setCount(count+1);
+  }
+
+  return (
+    <>
+      <div>
+      <Square value = {squares[0]} onBtnClick = {() => handleClick(0)}/>
+      <Square value = {squares[1]} onBtnClick = {() => handleClick(1)}/>
+      <Square value = {squares[2]} onBtnClick = {() => handleClick(2)}/>
+      </div>
+      <div>
+      <Square value = {squares[3]} onBtnClick = {() => handleClick(3)}/>
+      <Square value = {squares[4]} onBtnClick = {() => handleClick(4)}/>
+      <Square value = {squares[5]} onBtnClick = {() => handleClick(5)}/>
+      </div>
+      <div>
+      <Square value = {squares[6]} onBtnClick = {() => handleClick(6)}/>
+      <Square value = {squares[7]} onBtnClick = {() => handleClick(7)}/>
+      <Square value = {squares[8]} onBtnClick = {() => handleClick(8)}/>
+      </div>
+    </>
+  );
 }
